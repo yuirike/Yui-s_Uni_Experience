3677bfc55367877e327682b291a51844faf10018
Rainer Miila <shanrosan@gmail.com>
2022-12-14 08:18:44 +0100

    Works fine now

--- Ex16_Tuple_Dic_Lists/Reverse_Index/script.py
+++ Ex16_Tuple_Dic_Lists/Reverse_Index/script.py
@@ -1,17 +1,48 @@
 from collections import defaultdict
 
 # Dataset contains data that will be reverse indexed
+# dataset = [
+#     "Hello world",
+#     "This is the WORLD",
+#     "hello again"
+#  ] 
+
 dataset = [
-    "Hello world",
-    "This is the WORLD",
-    "hello again"
+    "Hello     world",
+    "This is the    WORLD",
+    "hello    again"
  ] 
 
+
 def reverse_index(dataset):
-    
+    data = [x.lower().split(" ")for x in dataset]
+    single = []
+    for i in data: single.extend(i)   
+    single = list(set(single))
+    if '' in single: single.remove('')
     index_dictionary = {}
+
+    for key_word in single:
+        index_list = []
+        for index_num,string_list in enumerate(data):
+            if key_word in string_list: index_list.append(index_num)
+            index_dictionary[key_word]=index_list
+    return index_dictionary
 
     # don't forget to return your resulting dictionary
 
 # You can see the output of your function here
 print(reverse_index(dataset))
+# for i in reverse_index(dataset).items():
+#     print(i)
+
+from their_script import reverse_index2
+dataset2 = [
+    "Hello     world",
+    "This is the    WORLD",
+    "hello    again"
+ ] 
+print(reverse_index2(dataset2))
+print(reverse_index(dataset) == reverse_index2(dataset2))
+
+
