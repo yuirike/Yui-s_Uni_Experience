75bd111a69a7e47a0462d11b17d6e309c8600611
Rainer Miila <shanrosan@gmail.com>
2022-12-15 08:16:21 +0100

    Should work now, hopefully. Adjusted the list comprehension, by making a list of the words that only contain hashtags and no other char... I think?

--- Ex17/Hashtag/script.py
+++ Ex17/Hashtag/script.py
@@ -1,24 +1,28 @@
+from their_script import analyze2
+
+
 def split_at(x):
     for i in x[1:]:
-        if not i.isalnum():return x[:x.index(i)]
+        if not i.isalnum():
+            return x[:x.index(i)]
+
 
 def analyze(posts):
-    str_to_list = [x.split() for x in posts] #turn the string into a nested list. 
+    # turn the string into a nested list.
+    str_to_list = [x.split() for x in posts]
     word_list = []
-    for i in str_to_list: word_list.extend(i) #get a list of words
-    keys = list([x for x in word_list if x[0]=='#' and x[1].isalpha()]) #make a list of keys
-    keys_without = list([x.replace('#','') for x in word_list if x[0]=='#' and x[1].isalpha()]) #make a list of keys without '#'
-    keys_without = [split_at(x) if not x.isalnum() else x for x in keys_without] #create appropriate keys
-    keys = [split_at(x) if not x[1:].isalnum() else x for x in keys]
-    keys = list(set(keys))
-    keys_without = list(set(keys_without))
+    for i in str_to_list:
+        word_list.extend(i)
+    only_hash = [x for x in word_list if '#' in x]
+    only_hash = [x[x.find('#'):] for x in only_hash]
+    only_hash = [x if x[1:].isalnum() else split_at(x) for x in only_hash]
+    keys = list(set([x[1:] for x in only_hash if x[1].isalpha]))
+    keys_with = list(set(x for x in only_hash if x[1].isalpha))
     keys.sort()
-    keys_without.sort()
-    #sort lists to let zip() do the magic.
-  
+    keys_with.sort()
     output = {}
-    for key,key_w in zip(keys, keys_without):
-        output[key_w]=str(word_list).count(key)
+    for a, b in zip(keys, keys_with):
+        output[a] = only_hash.count(b)
 
     return output
 
@@ -35,8 +39,6 @@ posts = [
 print(analyze(posts))
 
 
-
-from their_script import analyze2
+# Their script, for comparison.
 print(analyze2(posts))
 print(analyze(posts) == analyze2(posts))
-
