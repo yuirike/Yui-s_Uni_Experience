57f96170c86ed3301ef3f8932f90e1ad47bf6f88
Rainer Miila <shanrosan@gmail.com>
2022-12-17 17:23:32 +0100

    Mostly done, there is an issue, when the keywords aren't ordered by length.

--- Ex19_OOP/Profanity/script.py
+++ Ex19_OOP/Profanity/script.py
@@ -1,46 +1,67 @@
 #!/usr/bin/env python3
 
-# The signatures of this class and its public methods are required for the automated grading to work. 
-# You must not change the names or the list of parameters. 
+# The signatures of this class and its public methods are required for the automated grading to work.
+# You must not change the names or the list of parameters.
 # You may introduce private/protected utility methods though.
 class ProfanityFilter:
-
     def __init__(self, keywords, template):
-        self.__keywords = keywords
+        self.__keywords = sorted([x.lower() for x in keywords], key=len)
         self.__template = template
-    
-
 
     def filter(self, msg):
-        censor = []
-        c_len = len(self.__template)
-        pass
+        censor_dictionary = {}
+        censor = [x for x in self.__template]
+        censored_words = []
+        # key_w = self.__keywords.copy()
+ 
+        for word in self.__keywords:
+            censored_word = []  # for each word in keywords, create a list.
+            copy = censor.copy()  # make a copy of the censor
+            for letter in word:
+                # append for each letter in word, the first letter of copy (censor)
+                censored_word.append(copy[0])
+                copy.append(copy[0])  # append that letter to the end of copy
+                copy.pop(0)  # remove first letter of copy
+            # add that joined list to censored_words.
+            censored_words.append("".join(censored_word))
 
-            
+        for a, b in zip(self.__keywords, censored_words):
+            censor_dictionary[a] = b
 
+        new_msg = msg.lower()
+        for i in censor_dictionary.keys():
+            if i in new_msg:
+                new_msg = new_msg.replace(i, censor_dictionary[i])
+
+        output = []
+        for a, b in zip(msg, new_msg):
+            if a.lower() == b:
+                output.append(a)
+            else:
+                output.append(b)
+
+        output = "".join(output)
+        return output
 
 
 # You can play around with your implementation in the body of the following 'if'.
 # The contained statements will be ignored while evaluating your solution.
 if __name__ == '__main__':
-    f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+    f = ProfanityFilter(["mastard", "duck", "shot", "batch"], "?#$")
     offensive_msg = "abc defghi mastard jklmno"
     clean_msg = f.filter(offensive_msg)
     print(clean_msg)  # abc defghi ?#$?#$? jklmno
 
 
-#Figuring out repeated loops.
-tp = "kek"
-top = "MyBad"
-p =[]
-tp = [x for x in tp]
+# Figuring out repeated loops.
+censor = "kek"
+word = "MyBad"
+out = []
+censor = [x for x in censor]
 
-for i in top:
-    p.append(tp[0])
-    tp.append(tp[0])
-    tp.pop(0)
+for i in word:
+    out.append(censor[0])
+    censor.append(censor[0])
+    censor.pop(0)
 
-print(p)
-
-
-
+print(len(word) == len(out))
--- Ex19_OOP/Profanity/tests.py
+++ Ex19_OOP/Profanity/tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from unittest import TestCase
-from public.script import ProfanityFilter
+from script import ProfanityFilter
 
 
 class PublicTestSuite(TestCase):
@@ -12,6 +12,49 @@ class PublicTestSuite(TestCase):
         actual = f.filter(msg)
         expected = "abc defghi ?#$?#$? jklmno"
         self.assertEqual(expected, actual)
+
+    def test_New_Curses(self):
+        f = ProfanityFilter(["Slut", "Bitch","Whore","Shit","Cockless"], "---")
+        msg = "I really hate you. You're such a Slut. I can't believe how much of a bitch you've become."
+        actual = f.filter(msg)
+        expected = "I really hate you. You're such a ----. I can't believe how much of a ----- you've become."
+        self.assertEqual(expected, actual)
+
+    def test_Capital_cases(self):
+        f = ProfanityFilter(["BiTcH","Cock","Dick"], "?#$")
+        msg = "She's quite a bitch, isn't she? cock No wonder her boyfriend cheated on her with his massive dick."
+        actual = f.filter(msg)
+        expected = "She's quite a ?#$?#, isn't she? ?#$? No wonder her boyfriend cheated on her with his massive ?#$?."
+        self.assertEqual(expected, actual)
+
+    def test_Capital_cases2(self):
+        f = ProfanityFilter(["BiTcH","Cock","Dick"], "?#$")
+        msg = "She's quite a bitchy BITCH, isn't she? No wonder her boyfriend cheated on her with his massive dick, nay, I mean cock!"
+        actual = f.filter(msg)
+        expected = "She's quite a ?#$?#y ?#$?#, isn't she? No wonder her boyfriend cheated on her with his massive ?#$?, nay, I mean ?#$?!"
+        self.assertEqual(expected, actual)
+
+    def test_Capital_cases3(self):
+        f = ProfanityFilter(["Anus","Penis","Dick","Cock"], "@")
+        msg = "KLSJLQJDIJOOJDOQJPOLJFDLOanuskjdijofjaoöfjjfcock"
+        actual = f.filter(msg)
+        expected = "KLSJLQJDIJOOJDOQJPOLJFDLO@@@@kjdijofjaoöfjjf@@@@"
+        self.assertEqual(expected, actual)
+
+    def test_Capital_cases3(self):
+        f = ProfanityFilter(["Profit"], "*")
+        msg = "Our profit was really profitable today, I feel profitably good."
+        actual = f.filter(msg)
+        expected = "Our ****** was really ******able today, I feel ******ably good."
+        self.assertEqual(expected, actual)
+
+    def test_Subword(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "*")
+        msg = "I duckshot him in the face!"
+        actual = f.filter(msg)
+        expected = "I ******** him in the face!"
+        self.assertEqual(expected, actual)
+
 
     # This current test suite only contains one very basic test case. By now,
     # you have some experience in writing test cases. We strongly ecncourage
